deb212808595f3813ce286d459c4d5ed282e4f86
Blane <blane.clorley.09@aberdeen.ac.uk>
2011-05-06 23:14:42 +0100

    Intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArtefactApp::Application.config.secret_token = '3bf99d5bd02a620d15bb57df949426043976d76df0e07f0a8ac9a3fe61e74ad8dd24e722db4b1243e24d283453e0136c36ea77aebf507e670b574da8be8d0f00'
