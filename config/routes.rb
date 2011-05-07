7a0457f1cf648e0b1666463b4311ed44e06ad0d4
Blane <blane.clorley.09@aberdeen.ac.uk>
2011-05-07 02:51:13 +0100

    Removed default Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 ArtefactApp::Application.routes.draw do
-  get "pages/home"
-  get "pages/contact"
-  get "pages/about"
+      # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  # root :to => "welcome#index"
+
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  
+  root :to => 'pages#home'
+
   end
